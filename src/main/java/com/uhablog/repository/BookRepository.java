a2f5c47333b20a0b22047aba1e9474df0b03ba92
user <user@LAPTOP-VBOH87L6>
2025-08-15 15:04:46 +0900

    Repositoryインターフェースの作成

--- src/main/java/com/uhablog/repository/BookRepository.java
+++ src/main/java/com/uhablog/repository/BookRepository.java
@@ -1,5 +1,19 @@
 package com.uhablog.repository;
 
-public class BookRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.uhablog.model.Book;
+
+/* JpaRepositoryインターフェースを継承することで、以下のような基本的なデータの取得するメソッドを自動で作成してくれる。
+ * データの取得
+ * データの挿入
+ * データの更新
+ * データの削除
+ */
+/* Bookクラスを操作するためのRepositoryを作成するため、JpaRepository<Book, Integer>としている。
+ * IntegerはBookクラスのIDの型を指定している。
+ */
+public interface BookRepository extends JpaRepository<Book, Integer> {
+	
 
 }
