65c200ed55e13cc6819fa77ae4891f2e33baea20
user <user@LAPTOP-VBOH87L6>
2025-08-15 14:36:11 +0900

    新規作成

---
+++ src/test/java/com/uhablog/SpringSampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uhablog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
