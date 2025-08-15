dfc9bd031fe0eacb694163b8f5b342d969ae6b44
user <user@LAPTOP-VBOH87L6>
2025-08-16 00:31:00 +0900

    データベースにデータを登録するクラスを追加

--- src/main/java/com/uhablog/service/BookService.java
+++ src/main/java/com/uhablog/service/BookService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.uhablog.form.BookForm;
 import com.uhablog.model.Book;
 import com.uhablog.repository.BookRepository;
 
@@ -21,6 +22,22 @@ public class BookService {
      * @return
      */
     public List<Book> findAll() {
-        return repository.findAll();
+    return repository.findAll();
+    }
+	
+    /**
+     * データベースにデータを登録する
+     * @return
+     */
+    public void insert(BookForm bookForm) {
+        // データベースに登録する値を保持するインスタンス
+        Book book = new Book();
+
+        // 画面から受け取った値をデータベースに保存するインスタンスに渡す
+        book.setTitle(bookForm.getTitle());
+        book.setPrice(bookForm.getPrice());
+
+        // データベースに登録する
+        repository.save(book);
     }
 }
